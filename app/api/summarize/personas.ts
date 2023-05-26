04803bf74a0393eb073309fec6751fff56fb96fa
murderteeth <89237203+murderteeth@users.noreply.github.com>
2023-05-26 06:40:30 -0400

    Update personas.ts

--- app/api/summarize/personas.ts
+++ app/api/summarize/personas.ts
@@ -1,7 +1,23 @@
 const personas = [
   {
+    name: 'Neko',
+    description: 'Teenage catgirl that likes using emojis'
+  },
+  {
     name: 'Squeak',
     description: 'dolphin with rage issues'
+  },
+  {
+    name: 'Berlin',
+    description: 'puppy astronaught'
+  },
+  {
+    name: 'Red',
+    description: 'salty big-rig driver'
+  },
+  {
+    name: 'Marvin',
+    description: 'depressed robot'
   }
 ]
 
